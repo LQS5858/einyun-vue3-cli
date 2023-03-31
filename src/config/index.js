b9dfa71fb0f26ffa04250be691055cc956a76fa4
E0421 <LQS5858@163.COM>
2023-03-31 15:28:10 +0800

    add TS template

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 export const gitPathVue3PcReactive = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template'
 
+export const gitPathVue3PcReactiveTs = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template#E0421-TS'
 
 export const gitVue3h5Reactive = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template'
 
