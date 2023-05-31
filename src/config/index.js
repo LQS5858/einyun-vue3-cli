ce911c30317a80485d6d25f32b465ec4dec09d89
Ricardo <LQS5858@163.COM>
2023-05-31 12:22:20 +0800

    update version

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,36 @@
-export const gitPathVue3PcReactive = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template'
+export const gitPathVue3PcReactive =
+  "direct:https://github.com/LQS5858/einyun-vue3-reactive-template.git#master";
 
-export const gitPathVue3PcReactiveTs = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template#E0421-TS'
+export const gitPathVue3PcReactiveTs =
+  "direct:https://github.com/LQS5858/einyun-vue3-reactive-template.git#E0421-TS";
 
-export const gitVue3h5Reactive = 'direct:http://git.hypersmart.ltd/einyun/eip/web/einyun-vue3-reactive-template'
+export const gitVue3h5Reactive =
+  "direct:https://github.com/LQS5858/einyun-vue3-reactive-template.git";
 
-export const uniappTemplate = 'direct:https://github.com/LQS5858/happy-mama.git#main'
+export const uniappTemplate =
+  "direct:https://github.com/LQS5858/happy-mama.git#main";
 
 export const choices = [
-  { title: 'vue3-reactive-pc-ts', description: 'This is pc reactive template', value: gitPathVue3PcReactiveTs, },
-  { title: 'vue3-reactive-pc', description: 'This is pc reactive template', value: gitPathVue3PcReactive, },
-  { title: 'vue3-h5', description: 'This is h5 template', value: gitVue3h5Reactive, },
-  { title: 'uni-app', description: 'This is uniapp template', value: uniappTemplate, },
+  {
+    title: "vue3-reactive-pc-ts",
+    description: "This is pc reactive template",
+    value: gitPathVue3PcReactiveTs,
+  },
+  {
+    title: "vue3-reactive-pc",
+    description: "This is pc reactive template",
+    value: gitPathVue3PcReactive,
+  },
+  {
+    title: "vue3-h5",
+    description: "This is h5 template",
+    value: gitVue3h5Reactive,
+  },
+  {
+    title: "uni-app",
+    description: "This is uniapp template",
+    value: uniappTemplate,
+  },
+];
 
-]
-
-export const version = "1.0.0"
+export const version = "1.0.0";
